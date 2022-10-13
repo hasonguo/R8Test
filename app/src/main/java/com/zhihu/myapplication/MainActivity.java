933f9a0433e0ba658c00d01c732cd03b504eb277
guohaisheng <guohaisheng@zhihu.com>
2022-10-13 18:31:14 +0800

    first commit

---
+++ app/src/main/java/com/zhihu/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.zhihu.myapplication;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.zhihu.module2.Module2;
+
+
+//import com.zhihu.module2.Module2;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Module2 module2 = new Module2();
+        module2.method();
+
+    }
+
+
+}
